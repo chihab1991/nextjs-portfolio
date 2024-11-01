c9ddbaae89f1409674da498d1b1a49b2d70e6b5f
chihab1991 <chihab.chergui.1991@gmail.com>
2024-11-01 14:51:01 +0100

    fixes for first version before deploying

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 const About = () => {
 	return (
 		<section id="about" className="text-center px-6 py-20 container mx-auto">
@@ -10,28 +8,28 @@ const About = () => {
 				</span>
 			</h4>
 			<p className="pt-12 leading-8">
-				Hello! I’m a passionate React & Next.js Web Developer based in Algeria.
-				With a background in electrical engineering and a deep love for coding,
-				I’ve transitioned into web development to create clean, efficient, and
-				user-centric web applications. I specialize in building dynamic and
-				responsive websites using JavaScript, TypeScript, and modern frameworks
-				like React and Next.js.
+				Hello! I&apos;m a passionate React & Next.js Web Developer based in
+				Algeria. With a background in electrical engineering and a deep love for
+				coding, I&apos;ve transitioned into web development to create clean,
+				efficient, and user-centric web applications. I specialize in building
+				dynamic and responsive websites using JavaScript, TypeScript, and modern
+				frameworks like React and Next.js.
 			</p>
 			<p className="pt-12 leading-8">
-				I’m skilled in crafting intuitive front-end interfaces with Tailwind CSS
-				and developing robust back-end solutions using Node.js and Express.js.
-				My expertise extends to working with MongoDB, ensuring seamless data
-				management for full-stack applications.
+				I&apos;m skilled in crafting intuitive front-end interfaces with
+				Tailwind CSS and developing robust back-end solutions using Node.js and
+				Express.js. My expertise extends to working with MongoDB, ensuring
+				seamless data management for full-stack applications.
 			</p>
 			<p className="pt-12 leading-8">
-				Currently, I’m also teaching middle school, balancing both my passion
-				for education and web development. I’m on a journey to expand my
-				freelance work and bring impactful digital solutions to clients
-				globally.
+				Currently, I&apos;m also teaching middle school, balancing both my
+				passion for education and web development. I&apos;m on a journey to
+				expand my freelance work and bring impactful digital solutions to
+				clients globally.
 			</p>
 			<p className="pt-12 leading-8">
 				Feel free to explore my portfolio and reach out for collaboration
-				opportunities. Let’s create something amazing together!
+				opportunities. Let&apos;s create something amazing together!
 			</p>
 		</section>
 	);
